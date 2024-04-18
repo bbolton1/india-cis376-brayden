e3a82036dd61eeb1cdf4edab178c6dc57a91d084
Brayden Bolton <157667096+bbolton1@users.noreply.github.com>
2024-04-18 03:37:07 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('#character-form').submit(function(event) {
+    event.preventDefault();
+    let charName = $('#charName').val();
+    let charClass = $('input[name="charClass"]:checked').val();
+    let charRace = $('#charRace').val();
+    let charBackground = $('#charBackground').val();
+
+    // Do something with the character data, like displaying it
+    alert("Character Name: " + charName + "\nClass: " + charClass + "\nRace: " + charRace + "\nBackground: " + charBackground);
+
+    // You can send this data to a server or perform any other action here
+  });
+});
