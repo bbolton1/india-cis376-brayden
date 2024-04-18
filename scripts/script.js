49bc054b5e67819f1930a27e2837e8a5ae9d0afc
Brayden Bolton <157667096+bbolton1@users.noreply.github.com>
2024-04-18 12:30:34 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   $('#charRace').change(function(){
-    if($(this).val() === "Other"){
+    if($(this).val() === "OtherRace"){
       $('#otherRaceInput').show();
     } else {
       $('#otherRaceInput').hide();
@@ -14,12 +14,28 @@ $(document).ready(function() {
     let charClass = $('input[name="charClass"]:checked').val();
     let charRace = $('#charRace').val();
     let charBackground = $('#charBackground').val();
-
+  });
     // Do something with the character data, like displaying it
-    alert("Character Name: " + charName + "\nClass: " + charClass + "\nRace: " + charRace + "\nBackground: " + charBackground);
-
+    // alert("Character Name: " + charName + "\nClass: " + charClass + "\nRace: " + charRace + "\nBackground: " + charBackground);
     // You can send this data to a server or perform any other action here
+
+    // Clear the main content area
+    function clearMainContent() {
+      $('#main-content').empty();
+    }
+
+    // Example: Adding new content to the main content area
+    function addNewContent() {
+      $('#main-content').html('<h2>New Content Added</h2><p>This is some new content added dynamically.</p>');
+    }
+
+    // Example: Clearing content and adding new content when a button is clicked
+    $('#clearContent').click(function() {
+      clearMainContent();
+      addNewContent();
   });
 });
+
+
 
 
