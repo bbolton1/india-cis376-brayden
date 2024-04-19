c48a1a95ae5a612f06930772b19f3a2f85106a84
Brayden Bolton <157667096+bbolton1@users.noreply.github.com>
2024-04-19 03:59:22 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
   $('#charRace').change(function(){
     if($(this).val() === "OtherRace"){
       $('#otherRaceInput').show();
@@ -8,31 +7,45 @@ $(document).ready(function() {
     }
   });
 
+  function showMenu() {
+    var x = document.getElementById("links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
   $('#character-form').submit(function(event) {
     event.preventDefault();
     let charName = $('#charName').val();
     let charClass = $('input[name="charClass"]:checked').val();
     let charRace = $('#charRace').val();
-    let charBackground = $('#charBackground').val();
-  });
-    // Do something with the character data, like displaying it
+    let profession = $('#profession').val();
 
-    // Clear the main content area
-    function clearMainContent() {
-      $('#creationPage').empty();
-    }
 
-    // Example: Adding new content to the main content area
-    function showNewContent() {
-      $('#adventurePage').show();
-    }
+  // Clear the main content area
+  function clearMainContent() {
+    $('#creationPage').empty();
+  }
 
-    // Example: Clearing content and adding new content when a button is clicked
-    $('#clearContent').click(function() {
-      clearMainContent();
-      showNewContent();
+  function showNewContent() {
+    $('#adventurePage').show();
+  }
+
+  function makeBackgroundBlack() {
+    $('body').addClass('secondBackground');
+  }
+
+  // Example: Clearing content and adding new content when a button is clicked
+  $('#clearContent').click(function() {
+    clearMainContent();
+    showNewContent();
+    makeBackgroundBlack();
   });
 });
+});
+
 
 
 
